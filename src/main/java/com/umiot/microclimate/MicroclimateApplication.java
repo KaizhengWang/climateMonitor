93bbcdcd387de62dfe8fd19deac491073bab5a51
KaizhengWang <wkznico@163.com>
2026-03-13 13:43:21 +0800

    update MicroclimateApplication

--- src/main/java/com/umiot/microclimate/MicroclimateApplication.java
+++ src/main/java/com/umiot/microclimate/MicroclimateApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class MicroclimateApplication {
+public class MicroclimateApplication { // main class
 
     public static void main(String[] args) {
         SpringApplication.run(MicroclimateApplication.class, args);
